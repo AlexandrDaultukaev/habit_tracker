b1b4520a71c4b4438bedf2c16297c85a498cf156
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-29 20:43:02 +0700

    Create graph

--- main.py
+++ main.py
@@ -3,16 +3,27 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+TOKEN = os.getenv("TOKEN")
 
 user_params = {
-    "token": os.getenv("TOKEN"),
+    "token": TOKEN,
     "username": os.getenv("USERNAME"),
-    "agree": os.getenv("AGREE"),
-    "nminor": os.getenv("NMINOR"),
+    "agreeTermsOfService": os.getenv("AGREE"),
+    "notMinor": os.getenv("NMINOR"),
 }
 
-print(user_params)
+graph_params = {
+    "id": os.getenv("IDG"),
+    "name": os.getenv("NM"),
+    "unit": os.getenv("UNT"),
+    "type": os.getenv("TYP"),
+    "color": os.getenv("CLR"),
+}
 
-response = requests.post(url="https://pixe.la/v1/users", json=user_params)
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+#response = requests.post(url="https://pixe.la/v1/users", json=user_params)
+#response = requests.post(url=f"https://pixe.la//v1/users/{user_params['username']}/graphs", json=graph_params, headers=headers)
 print(response.text)
