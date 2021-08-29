030a558509977160440ac878985e12cd4b187228
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-29 20:58:33 +0700

    Add pixel to the graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,6 +25,15 @@ headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
+now = str(datetime.datetime.now()).split()[0].replace("-", "")
+
+pixel_params = {
+    "date": now,
+    "quantity": input("How much time did you study today?\n"),
+}
+
 #response = requests.post(url="https://pixe.la/v1/users", json=user_params)
 #response = requests.post(url=f"https://pixe.la//v1/users/{user_params['username']}/graphs", json=graph_params, headers=headers)
+response = requests.post(url=f"https://pixe.la//v1/users/{user_params['username']}/graphs/{graph_params['id']}",
+                         json=pixel_params, headers=headers)
 print(response.text)
